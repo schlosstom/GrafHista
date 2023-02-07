e6e3fb2566f33117e6d42fabe8732949bace43ee
Thomas Schlosser <thomas.schlosser@suse.com>
2023-02-07 18:08:16 +0100

    code improvements

--- create_sar_db.py
+++ create_sar_db.py
@@ -1,29 +1,33 @@
 #! /usr/bin/python
-# ------------------------------------------------------------------------------
-# Copyright (c) 2023 SUSE LLC
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of version 3 of the GNU General Public License as published by the
-# Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, contact SUSE Linux GmbH.
-# 
-# ------------------------------------------------------------------------------
-# Author: Thomas Schlosser <thomas.schlosser@suse.com>
-# 
-# create_sar_db
-#
-# Imports data of a sar binary file and creates a mysql db. The mysql db can then
-# be included into a Grafana dashboard for creating further queries and metrics.
-#
-# Changelog:    2023-01-30  v0.1    First testing.
-#               2023-02-01  v0.11   Each sar file has it own DB based on it name.
-#               2023-02-07  v0.2    Code improvements
+
+"""
+------------------------------------------------------------------------------
+Copyright (c) 2023 SUSE LLC
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of version 3 of the GNU General Public License as published by the
+Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, contact SUSE Linux GmbH.
+ 
+------------------------------------------------------------------------------
+Author: Thomas Schlosser <thomas.schlosser@suse.com>
+ 
+create_sar_db.py
+
+Imports data of a sar binary file and creates a mysql db. The mysql db can then
+be included into a Grafana dashboard for creating further queries and metrics.
+
+Changelog:    2023-01-30  v0.1    First testing.
+              2023-02-01  v0.11   Each sar file has it own DB based on it name.
+              2023-02-07  v0.2    Code improvements
+"""
+
 
 import subprocess
 import mysql.connector
@@ -158,21 +162,25 @@ def create_insert_string(data_set):
     return(sql_string)
 
 
+def create_tables(file, options):
+    for option in options:
+        table_dict = capture_data(file, option)
+
+        # Create table
+        sql = create_table_string(table_dict[0])
+        MY_CURSOR.execute(sql)
+
+        # Insert data 
+        for data_set in table_dict:
+            sql = create_insert_string(data_set)
+            MY_CURSOR.execute(sql)          
+        my_db.commit()
+
 
  #### Main ######################################
 
 create_db(args.file)
+create_tables(args.file,OPTIONS)
 
-for option in OPTIONS:
-    table_dict = capture_data(args.file, option)
 
-    # Create table
-    sql = create_table_string(table_dict[0])
-    MY_CURSOR.execute(sql)
-
-    # Insert data 
-    for data_set in table_dict:
-        sql = create_insert_string(data_set)
-        MY_CURSOR.execute(sql)
-        my_db.commit()
 
