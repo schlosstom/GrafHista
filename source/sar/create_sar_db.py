e0c0886c17db19a54372495cead63d06eca90a99
Thomas Schlosser <thomas.schlosser@suse.com>
2023-04-05 13:34:38 +0200

    rewriten code to get option dev working

--- source/sar/create_sar_db.py
+++ source/sar/create_sar_db.py
@@ -32,6 +32,8 @@ Changelog:  2023-01-30  v0.1    First testing.
             2023-03-02  v0.9    - add config.yaml
                                 - some error handling improvements (still not satisfied)
                                 - promtail.yml improvements
+            2023-04-05  v0.95   Changed the logic to be able capturing single dev and CPUs as well.
+                                Some additional improvements.
 
 """
 
@@ -86,15 +88,10 @@ def capture_data(file, option):
 
     return(path)
 
-
-
-def create_db(file, source_key):
+def create_db(sar_file, path_name, hostname):
     """ Create DATABASE based on the sar file name """
     try:
-        # We need the hostname 
-        hostname = capture_data(file, "-r")['nodename']
-
-        database_name = hostname + "_" + source_key + "_" + Path(file).name
+        database_name = hostname + "_" + path_name + "_" + Path(sar_file).name
         sql = "DROP DATABASE IF EXISTS " + database_name
         MY_CURSOR.execute(sql)
 
@@ -103,69 +100,121 @@ def create_db(file, source_key):
 
         sql = "use " + database_name
         MY_CURSOR.execute(sql)
-        logging.info(f'create_db() - Created database: {database_name}')
+        print("Create DB: ",database_name)
     except:
-        logging.error(f'create_db() - Error creating or dropping database {file} for hostname {hostname}')
+        logging.error(f'create_db() - Error creating or dropping database {sar_file} for hostname {hostname}')
         sys.exit(1)
 
 
-
-def create_tables(file):
-    """ Main function for creating the database and all tables based on the given sar file """
+def create_table(key_name, item):
+    """ Create DATABASE table for a single item (e.g. dev, memory, ...)"""
     try:
-        for option in config['options'].values():
-            table_dict = capture_data(file,option)['statistics']
-
-            # Create table
-            sql = sar_db.create_table_string(table_dict[0])
-            logging.debug(f'create_tables() - create SQL string is: {sql}')
-            MY_CURSOR.execute(sql)
-
-            # Insert data
-            for data_set in table_dict:
-                sql = sar_db.create_insert_string(data_set)
-                logging.debug(f'create_tables() - Insert SQL is is: {sql}')
-                MY_CURSOR.execute(sql)
-
-            db_connect.commit()
+        # Unfortunately swap and memory have the same key_name "memory"
+        # If there is a data key swapfree we will remane the key_name to "swap"    
+        if 'swpfree'  in item.keys():
+            key_name = "swap"
+        #key_name = sar_db.name_correction(key_name)
+        sql_string = "CREATE TABLE " + key_name + " (Date DATETIME"
+        for key, value in item.items():
+            #key = sar_db.name_correction(key)
+            sql_string += ", " + key + sar_db.data_type(value)
+        sql_string += ")"
+        sql = sar_db.name_correction(sql_string)
+        print(sql)
+        MY_CURSOR.execute(sql)
     except:
-        logging.error(f'create_tables() - Error creating tables!')
+        logging.error(f'create_table() - Error creating table {key_name} ')
+        sys.exit(1)            
 
+
+
+def insert_table(key_name, item, time_line):
+    """ Insert data for a single item (e.g. dev, memory, ...)"""
+    try:
+        # Unfortunately swap and memory have the same key_name "memory"
+        # If there is a data key swapfree we will remane the key_name to "swap"    
+        if 'swpfree'  in item.keys():
+            key_name = "swap"
+
+        date = sar_db.get_date_time(time_line)
+        sql_string = "INSERT INTO " + key_name + " SET Date = \'" + date  + "\'"
+        for key, value in item.items():
+            #key = sar_db.name_correction(key)
+            sql_string += ", " + str(key) + " = \'" + str(value) + "\' "
+        sql = sar_db.name_correction(sql_string)
+        print(sql)
+        MY_CURSOR.execute(sql)
+    except:
+        logging.error(f'insert_table() - Error inserting {key_name} for date {date}')
+        sys.exit(1)    
+
+
+def prepare_tables(sar_file):
+    """ Preparing the sar_file data for the tables  """
+    try:
+        # For each item (option)
+        for option in config['options'].values():
+            dict_option = capture_data(sar_file,option)['statistics']
+            create_table_flac  = True
+            # For every timeline 
+            for time_line in dict_option:
+                key_name = (set(time_line.keys()) - {'timestamp'}).pop()
+                # Most items has only one "device" like CPU(ALL) or memory, etc.
+                # This can be seen by the type which is in that case a dictionary part.
+                if isinstance(time_line[key_name], dict):
+                    item = time_line[key_name]
+                    # Make sure the table will be created only once. 
+                    if create_table_flac:
+                        create_table(key_name, item)
+                        create_table_flac  = False
+                    insert_table(key_name, item, time_line)
+                # Some items like dev or (single) CPU's have more items based on one timeline
+                # This can be seen by the type list. In this case an additional for loop is needed.
+                elif isinstance(time_line[key_name], list):
+                    item = time_line[key_name][0]
+                    # Make sure the table will be created only once. 
+                    if create_table_flac:    
+                        create_table(key_name, item)
+                        create_table_flac  = False
+                    for item in time_line[key_name]:
+                        insert_table(key_name, item, time_line)
+        db_connect.commit()
+    except:
+        logging.error(f'prepare_tables() - Error on preparing tables Option: {option} Keyname: {key_name} ')
+        sys.exit(1)        
 
 
 def load_data():
     """ Main process for loading all available sar files"""
     logging.info('load_data() - Starting load_loop()')
 
-    for source_key, path_value in config['sar_path'].items():
+    # For each path written in config file.
+    for path_name, path_value in config['sar_path'].items():
+        sar_path = glob.glob(path_value + "sa????????")
 
-        try:
-            sar_files = glob.glob(path_value + "sa????????")
-            for file in sar_files:
-                logging.debug(f'load_data() - Found file: {file}')
-                create_db(file, source_key)
-                create_tables(file)
-        except:
-            logging.warn(f'load_data() - Warn: An error occured collecting sar file {file}')
+        # For each sar file in given path.
+        for sar_file in sar_path:
+            host_name = capture_data(sar_file, "-r")['nodename']
+            create_db(sar_file, path_name, host_name)
+            prepare_tables(sar_file)
 
-    logging.info(f'load_data() - Waiting for new events ...')
 
 
 def main():
     """ Triggers the function load_data() if there are any changes in logs/ folder"""
-    logging.info(f'main() - Starting main()')
-    try:
-        load_data()
-        i = inotify.adapters.InotifyTree('/logs')
+    # First run on startup
+    load_data()
+    
+    # monitor the /logs folder inside the container 
+    i = inotify.adapters.InotifyTree('/logs')
 
-        for event in i.event_gen(yield_nones=False):
-            (_, type_names, _, _) = event
+    for event in i.event_gen(yield_nones=False):
+        (_, type_names, _, _) = event
 
-            if 'IN_CREATE' in type_names or 'IN_DELETE' in type_names:
-                logging.info(f'main() - Event action: sar content has been changed. Reload data...')
-                load_data()
-    except:
-        logging.info(f'main() - Error main function!')
+        # Triggers the main function load_data() by any new or deleted files or folders 
+        if 'IN_CREATE' in type_names or 'IN_DELETE' in type_names:
+            load_data()
+
 
 
 if __name__ == "__main__":
--- source/sar/sar_db.py
+++ source/sar/sar_db.py
@@ -14,69 +14,13 @@ def data_type(value):
         return(" VARCHAR(255)")
     elif isinstance(value, int):
         return(" BIGINT")
-
+ 
 
 def name_correction(name):
     """ Correct some "not allowed" names in mysql"""
-    if '-' in name:
-        name = name.replace('-','_')
-
-    if name == 'system':
-            name += '_'
+    name = name.replace('-','_')
+    name = name.replace('system', 'system_')
 
     return(name)
-
-
-def get_data_and_path(data_set):
-    """ Returns the key address and the name from the given index  """
-
-    key_name = (set(data_set.keys()) - {'timestamp'}).pop()
-
-    # Some data like cpu_load are stored in a single list element
-    # We check for list and assume that there is only one list element [0].
-    if isinstance(data_set[key_name], dict):
-        value = data_set[key_name]
-    elif isinstance(data_set[key_name], list):
-        value = data_set[key_name][0]
-    else:
-        logging.error('get_data_and_path() - An error occures. Exit')
-        sys.exit(1)
-
-    # Unfortunately swap and memory have the same key_name "memory"
-    # If there is a data key swapfree we will remane the key_name to "swap"
-    if 'swpfree'  in value.keys():
-        key_name = "swap"
-
-    return  name_correction(key_name), value
-
-
-def create_table_string(data_set):
-    """ Returns the sql string for creating the table based on the given index """
-    name, path = get_data_and_path(data_set)
-
-    sql_string = "CREATE TABLE " + name + " (Date DATETIME"
-
-    for key, value in path.items():
-        key = name_correction(key)
-        sql_string += ", " + key + data_type(value)
-
-    sql_string += ")"
-
-########    logging.info(f'create_table_string() - Creating table {sql_string}')
-    return(sql_string)
-
-
-def create_insert_string(data_set):
-    """ Returns the sql string for inserting proper data to the table based on the given index """
-    date = get_date_time(data_set)
-    name, path = get_data_and_path(data_set) 
-
-    sql_string = "INSERT INTO " + name + " SET Date = \'" + date  + "\'"
-
-    for key, value in path.items():
-        key = name_correction(key)
-        sql_string += ", " + str(key) + " = \'" + str(value) + "\' "
-
-    return(sql_string)
 
 
